d49f915b141f7a6e2e0b5a2550c9b3946c93ca0f
Doug Chamberlain <dougrchamberlain@gmail.com>
2018-08-08 22:16:17 -0400

    trying to merge my changes

---
+++ App/GameObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Oregon
+{
+    public abstract class GameObject 
+    {
+        public InputManager Input = new InputManager();
+        public readonly List<GameObject> Components = new List<GameObject>();
+
+        public GameObject()
+        {
+            //wire up event listeners
+            UpdateService.UpdateEvent += this.OnUpdateEvent;
+            InputManager.KeyPressEvent += this.OnKeyPressEvent;
+        }
+
+        private void OnUpdateEvent()
+        {
+            Type thisType = this.GetType();
+            MethodInfo invokable = thisType.GetMethod("Update");
+            invokable?.Invoke(this, null);
+        }
+
+        private void OnKeyPressEvent(InputManager.KeyPressEventArgs e)
+        {
+            InputManager.CurrentKey = e.KeyInfo;
+            
+            Type thisType = this.GetType();
+            MethodInfo invokable = thisType.GetMethod("OnKeyPress");
+            invokable?.Invoke(this, null);
+            InputManager.CurrentKey = new ConsoleKeyInfo();
+    }
+    }
+
+}
+
---
+++ App/UpdateService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Oregon
+{
+    public class UpdateService
+    {
+        public List<GameObject> GameObjects = new List<GameObject>();
+        public static event UpdateEventHandler UpdateEvent;
+        public delegate void UpdateEventHandler();
+
+
+        public void Init()
+        {
+            GameObjects.ForEach((item) =>
+            {
+                Type thisType = item.GetType();
+                MethodInfo invokable = thisType.GetMethod("Start");
+                invokable?.Invoke(item, null);
+            });
+
+        }
+
+        public void Update()
+        {
+            UpdateEvent.Invoke();
+            
+
+
+        }
+
+    }
+
+}
+
